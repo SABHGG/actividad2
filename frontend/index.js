573d44a9cecada913e0ce3383cb14834393612e8
Sergio Beleño <sergiosabh05@gmail.com>
2024-10-04 21:13:10 -0500

    Update frontend to include additional operations and improve UI elements

--- frontend/index.js
+++ frontend/index.js
@@ -1,99 +1,136 @@
-const contenido = document.querySelector('#contenido');
-const t1 = document.querySelector('#t1')
-const t2 = document.querySelector('#t2')
-const t3 = document.querySelector('#t3')
+const contenido = document.querySelector("#contenido");
+const t1 = document.querySelector("#t1");
+const t2 = document.querySelector("#t2");
 
-const op1 = document.querySelector('#opcion1')
-const op2 = document.querySelector('#opcion2')
-const op3 = document.querySelector('#opcion3')
-const op4 = document.querySelector('#opcion4')
+const op1 = document.querySelector("#opcion1");
+const op2 = document.querySelector("#opcion2");
+const op3 = document.querySelector("#opcion3");
+const op4 = document.querySelector("#opcion4");
+const op5 = document.querySelector("#opcion5");
+const op6 = document.querySelector("#opcion6");
+const op7 = document.querySelector("#opcion7");
 
-const btn = document.querySelector('#enviar')
+const btn = document.querySelector("#enviar");
 
-var url = 'https://actividad-1-backend.onrender.com'
-op1.addEventListener('click', opcion1)
-op2.addEventListener('click', opcion2)
-op3.addEventListener('click', opcion3)
-op4.addEventListener('click', opcion4)
+op1.addEventListener("click", opcion1);
+op2.addEventListener("click", opcion2);
+op3.addEventListener("click", opcion3);
+op4.addEventListener("click", opcion4);
+op5.addEventListener("click", opcion5);
+op6.addEventListener("click", opcion6);
+op7.addEventListener("click", opcion7);
 
-btn.addEventListener('click', enviar)
+btn.addEventListener("click", enviar);
 
-function opcion1(){
-    url = 'https://actividad-1-backend.onrender.com'
-    t1.disabled = true
-    t2.disabled = true
-    t3.disabled = true
+var url = "https://actividad2.onrender.com";
 
-    t1.value = ''
-    t2.value = ''
-    t3.value = ''
+t1.placeholder = "Ingrese el primer número";
+t2.placeholder = "Ingrese el segundo número";
 
-    t1.placeholder = ''
-    t2.placeholder = ''
-    t3.placeholder = ''
+function opcion1() {
+  url = "https://actividad2.onrender.com";
+  t1.disabled = false;
+  t2.disabled = false;
+
+  t1.value = "";
+  t2.value = "";
+
+  t1.placeholder = "Ingrese el primer número";
+  t2.placeholder = "Ingrese el segundo número";
 }
 
-function opcion2(){
-    url = 'https://actividad-1-backend.onrender.com'
-    t1.disabled = false
-    t2.disabled = false
-    t3.disabled = false
+function opcion2() {
+  url = "https://actividad2.onrender.com";
+  t1.disabled = false;
+  t2.disabled = false;
 
-    t1.value = ''
-    t2.value = ''
-    t3.value = ''
-    
-    t1.placeholder = 'Nota 1'
-    t2.placeholder = 'Nota 2'
-    t3.placeholder = 'Nota 3'
+  t1.value = "";
+  t2.value = "";
+
+  t1.placeholder = "Ingrese el primer número";
+  t2.placeholder = "Ingrese el segundo número";
 }
 
-function opcion3(){
-    url = 'https://actividad-1-backend.onrender.com'
+function opcion3() {
+  url = "https://actividad2.onrender.com";
+  t1.disabled = false;
+  t2.disabled = false;
 
-    t1.disabled = false
-    t2.disabled = true
-    t3.disabled = true
+  t1.value = "";
+  t2.value = "";
 
-    t1.value = ''
-    t2.value = ''
-    t3.value = ''
-
-    t1.placeholder = 'Ingrese su edad'
-    t2.placeholder = ''
-    t3.placeholder = ''
+  t1.placeholder = "Ingrese el primer número";
+  t2.placeholder = "Ingrese el segundo número";
 }
 
-function opcion4(){
-    url = 'https://actividad-1-backend.onrender.com'
-    t1.disabled = false
-    t2.disabled = false
-    t3.disabled = false
+function opcion4() {
+  url = "https://actividad2.onrender.com";
+  t1.disabled = false;
+  t2.disabled = false;
 
-    t1.value = ''
-    t2.value = ''
-    t3.value = ''
+  t1.value = "";
+  t2.value = "";
 
-    t1.placeholder = 'Valores permitidos'
-    t2.placeholder = 'Columnas'
-    t3.placeholder = 'Filas'
-}   
+  t1.placeholder = "Ingrese el primer número";
+  t2.placeholder = "Ingrese el segundo número";
+}
 
-function enviar(){
-    url = 'https://actividad-1-backend.onrender.com'
-    if(op2.checked){
-        url = url + `/notas/${parseFloat(t1.value).toFixed(1)}/${parseFloat(t2.value).toFixed(1)}/${parseFloat(t3.value).toFixed(1)}`
-        console.log(url)
-    }
-    else if(op3.checked){
-        url = url + `/edad/${t1.value}`
-    }
-    else if(op4.checked){
-        url = url + `/arreglo/${t1.value}/${t2.value}/${t3.value? t3.value : 0}`
-    }
-    fetch(url)
-    .then(res => res.text())
-    .then(data => {
-        contenido.innerHTML = data
-    }).catch(err => contenido.innerHTML = 'Error de conexión')
+function opcion5() {
+  url = "https://actividad2.onrender.com";
+  t1.disabled = false;
+  t2.disabled = false;
+
+  t1.value = "";
+  t2.value = "";
+
+  t1.placeholder = "Ingrese la base";
+  t2.placeholder = "Ingrese el exponente";
+}
+
+function opcion6() {
+  url = "https://actividad2.onrender.com";
+  t1.disabled = false;
+  t2.disabled = true;
+
+  t1.value = "";
+  t2.value = "";
+
+  t1.placeholder = "Ingrese el número";
+  t2.placeholder = "";
+}
+function opcion7() {
+  url = "https://actividad2.onrender.com";
+  t1.disabled = false;
+  t2.disabled = true;
+
+  t1.value = "";
+  t2.value = "";
+
+  t1.placeholder = "Ingrese el número";
+  t2.placeholder = "";
+}
+
+function enviar() {
+  url = "https://actividad2.onrender.com";
+  if (op1.checked) {
+    url = url + `/suma/${t1.value}/${t2.value}`;
+  } else if (op2.checked) {
+    url = url + `/resta/${t1.value}/${t2.value}`;
+  } else if (op3.checked) {
+    url = url + `/divicion/${t1.value}/${t2.value}`;
+  } else if (op4.checked) {
+    url = url + `/multiplicacion/${t1.value}/${t2.value}`;
+  } else if (op5.checked) {
+    url = url + `/potencia/${t1.value}/${t2.value}`;
+  } else if (op6.checked) {
+    url = url + `/seno/${t1.value}`;
+  } else if (op7.checked) {
+    url = url + `/coseno/${t1.value}`;
+  }
+  fetch(url)
+    .then((res) => res.json())
+    .then((data) => {
+      contenido.innerHTML = `El resultado es: ${data.resultado} <br/> Operación realizada: ${data.operacion}`;
+    })
+    .catch((err) => (contenido.innerHTML = "Error de conexión"));
 }
